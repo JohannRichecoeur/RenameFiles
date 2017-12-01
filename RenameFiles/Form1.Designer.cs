7af329100f7e11a01d3b99599b2c3e34b49a7dac
Johann Richecoeur <johann.richecoeur@outlook.com>
2017-12-01 14:45:32 +0100

    Initial commit

---
+++ RenameFiles/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RenameFiles
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button folderButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button renameButton;
+        private System.Windows.Forms.Button clearButton;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">True if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.renameButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.ProcessingLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderButton
+            // 
+            this.folderButton.Location = new System.Drawing.Point(12, 12);
+            this.folderButton.Name = "folderButton";
+            this.folderButton.Size = new System.Drawing.Size(75, 23);
+            this.folderButton.TabIndex = 0;
+            this.folderButton.Text = "Select folder";
+            this.folderButton.UseVisualStyleBackColor = true;
+            this.folderButton.Click += new System.EventHandler(this.FolderButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "selected folder";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(15, 90);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(888, 181);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // renameButton
+            // 
+            this.renameButton.Location = new System.Drawing.Point(790, 12);
+            this.renameButton.Name = "renameButton";
+            this.renameButton.Size = new System.Drawing.Size(113, 50);
+            this.renameButton.TabIndex = 3;
+            this.renameButton.Text = "Rename";
+            this.renameButton.UseVisualStyleBackColor = true;
+            this.renameButton.Click += new System.EventHandler(this.RenameButtonClick);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(15, 277);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButtonClick);
+            // 
+            // ProcessingLabel
+            // 
+            this.ProcessingLabel.AutoSize = true;
+            this.ProcessingLabel.Location = new System.Drawing.Point(15, 66);
+            this.ProcessingLabel.Name = "ProcessingLabel";
+            this.ProcessingLabel.Size = new System.Drawing.Size(35, 13);
+            this.ProcessingLabel.TabIndex = 5;
+            this.ProcessingLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(915, 312);
+            this.Controls.Add(this.ProcessingLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.renameButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folderButton);
+            this.Name = "Form1";
+            this.Text = "Rename Files";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ProcessingLabel;
+    }
+}
---
+++ RenameFiles/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RenameFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += this.Form1DragEnter;
+            this.dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView.Columns.Add(new DataGridViewColumn(new DataGridViewTextBoxCell()));
+            this.dataGridView.Columns.Add(new DataGridViewColumn(new DataGridViewTextBoxCell()));
+
+            this.ProcessingLabel.Text = "";
+        }
+
+        private void FolderButtonClick(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                var folder = this.folderBrowserDialog.SelectedPath;
+                var folderName = folder.Split('\\').Last();
+                this.FillFiles(Directory.GetFiles(folder), folderName);
+            }
+        }
+
+        private void RenameButtonClick(object sender, EventArgs e)
+        {
+            // First use basic file names to avoid conflicts
+            var count = 0;
+            foreach (var g in from DataGridViewRow g in this.dataGridView.Rows where g.Cells[0].Value != null && g.Cells[1].Value != null select g)
+            {
+                var fileName = (string)g.Cells[0].Value;
+                File.Move(fileName, fileName + "__" + count);
+                count++;
+            }
+
+            // Then update to the final name
+            count = 0;
+            foreach (var g in from DataGridViewRow g in this.dataGridView.Rows where g.Cells[0].Value != null && g.Cells[1].Value != null select g)
+            {
+                ProcessingLabel.Text = count + 1 + " sur " + (this.dataGridView.Rows.Count - 1);
+                ProcessingLabel.Refresh();
+
+                File.Move((string)g.Cells[0].Value + "__" + count, (string)g.Cells[1].Value);
+                count++;
+            }
+        }
+
+        private void ClearButtonClick(object sender, EventArgs e)
+        {
+            this.dataGridView.Rows.Clear();
+        }
+
+        private void Form1DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void Form1DragDrop(object sender, DragEventArgs e)
+        {
+            var folders = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var folder in folders)
+            {
+                if (!folder.ToUpper().EndsWith(".JPG"))
+                {
+                    this.FillFiles(Directory.GetFiles(folder), folder.Split('\\').Last());
+                }
+                else
+                {
+                    this.label1.Text = "Add a folder only, no files alone";
+                    break;
+                }
+            }
+
+        }
+
+        private void FillFiles(string[] filePaths, string folderName)
+        {
+            this.label1.Text = folderName;
+
+            var counter = 1;
+            foreach (var filePath in filePaths)
+            {
+                ProcessingLabel.Text = counter + " sur " + filePaths.Count();
+                ProcessingLabel.Refresh();
+
+                string fileName = filePath;
+
+                string prefix = folderName.Split('-').First() + "-";
+                string file = fileName.Split('\\').Last();
+                string path = fileName.Replace(file, "");
+
+                string extension = "." + file.Split('.').Last();
+
+                string zeroToAdd = "";
+
+                if (filePaths.Count() > 100)
+                {
+                    if (counter < 10)
+                    {
+                        zeroToAdd = "00";
+                    }
+                    else if (counter < 100)
+                    {
+                        zeroToAdd = "0";
+                    }
+                    else
+                    {
+                        zeroToAdd = "";
+                    }
+                }
+                else if (filePaths.Count() >= 10)
+                {
+                    zeroToAdd = counter < 10 ? "0" : "";
+                }
+                else if (filePaths.Count() < 11)
+                {
+                    zeroToAdd = "0";
+                }
+
+                file = prefix + zeroToAdd + counter;
+
+                this.dataGridView.Rows.Add(fileName, path + file + extension);
+                counter++;
+            }
+        }
+    }
+}
